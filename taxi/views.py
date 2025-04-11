124b57e8473eca06236067a2559f2d038e37acc2
Nikita Tkachenko <7heswatgg@gmail.com>
2025-04-11 08:50:49 +0200

    Fix: flake8 whitespaces

--- taxi/views.py
+++ taxi/views.py
@@ -102,8 +102,6 @@ class DriverDeleteView(LoginRequiredMixin, generic.DeleteView):
     success_url = reverse_lazy("taxi:driver-list")
 
 
-
-
 class DriverLicenceUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Driver
     form_class = DriverLicenseUpdateForm
